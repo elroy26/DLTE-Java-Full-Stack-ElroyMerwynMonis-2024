b776f789e3e0991fba99d93b0511bf284e2e029c
elroy26 <elroymerwyn@gmail.com>
2024-04-18 15:54:21 +0530

    finished the task for rest

--- DLTE-MyBank-Project/MyBank-Insurance-WebService/src/main/java/mybank/insurance/webservice/security/configs/InsuranceSecurity.java
+++ DLTE-MyBank-Project/MyBank-Insurance-WebService/src/main/java/mybank/insurance/webservice/security/configs/InsuranceSecurity.java
@@ -17,6 +17,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
 @Configuration
 @ComponentScan("maybank.insurance.dao")
 public class InsuranceSecurity {
@@ -38,8 +40,8 @@ public class InsuranceSecurity {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5502"));
-        configuration.addAllowedOrigin("http://127.0.0.1:5502");
+        configuration.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5502"));
+
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
@@ -57,7 +59,8 @@ public class InsuranceSecurity {
                 .failureHandler(failureHandler)
                 .successHandler(successHandler);
         httpSecurity.csrf().disable();
-
+        httpSecurity.cors();
+        
         httpSecurity.authorizeRequests().antMatchers("/profile/register").permitAll();
         httpSecurity.authorizeRequests().antMatchers("/v3/api-docs").permitAll();
 //        httpSecurity.authorizeRequests().antMatchers("/insurancerepo/insurance.wsdl").permitAll();
