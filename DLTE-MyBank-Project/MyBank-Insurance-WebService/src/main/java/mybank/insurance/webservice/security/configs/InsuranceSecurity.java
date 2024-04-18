e948f210809a6e8e5e2cb896e4bb41bb7e0ae972
elroy26 <elroymerwyn@gmail.com>
2024-04-18 15:07:57 +0530

    finished the task with rest frontend integration

--- DLTE-MyBank-Project/MyBank-Insurance-WebService/src/main/java/mybank/insurance/webservice/security/configs/InsuranceSecurity.java
+++ DLTE-MyBank-Project/MyBank-Insurance-WebService/src/main/java/mybank/insurance/webservice/security/configs/InsuranceSecurity.java
@@ -13,6 +13,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @ComponentScan("maybank.insurance.dao")
@@ -30,6 +33,20 @@ public class InsuranceSecurity {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOriginPatterns(Arrays.asList("http://127.0.0.1:5502"));
+        configuration.addAllowedOrigin("http://127.0.0.1:5502");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
