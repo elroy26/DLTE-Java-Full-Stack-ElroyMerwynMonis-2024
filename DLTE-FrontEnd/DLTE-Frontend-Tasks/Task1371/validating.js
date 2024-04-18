697785c4abe22d850a14db9201c8a24b023044cf
elroy26 <elroymerwyn@gmail.com>
2024-04-18 12:22:51 +0530

    finished the form validation using js

---
+++ DLTE-FrontEnd/DLTE-Frontend-Tasks/Task1371/validating.js
@@ -0,0 +1,74 @@
+const performValidate = () => {
+    var isValid = true;
+
+    var myForm = document.forms['application'];
+    const accountNumber = myForm.accountNumber.value.trim();
+    const accountHolder = myForm.accountHolder.value.trim();
+    const chequeBookType = myForm.chequeBookType.value;
+    const dateOfApply = myForm.dateOfApply.value;
+    const address = myForm.address.value.trim();
+    const signature = myForm.signature.value.trim();
+    const contactNumber = myForm.contactNumber.value.trim();
+    const email = myForm.email.value.trim();
+
+    var accountNumberErr = document.getElementById('accountNumbererr');
+    var accountHolderErr = document.getElementById('accountHoldererr');
+    var chequeBookTypeErr = document.getElementById('chequeBookTypeerr');
+    var dateOfApplyErr = document.getElementById('dateOfApplyerr');
+    var addressErr = document.getElementById('addresserr');
+    var signatureErr = document.getElementById('signatureerr');
+    var contactNumberErr = document.getElementById('contactNumbererr');
+    var emailErr = document.getElementById('emailerr');
+
+    try {
+        if (!(/[0-9]{12}/).test(accountNumber)) {
+            throw "Please enter account number";
+        }
+        accountNumberErr.innerText = '';
+    } catch (error) {
+        accountNumberErr.innerText = error;
+        isValid = false;
+    }
+
+    try {
+        if (!(/^[a-zA-Z ]+$/).test(accountHolder)) {
+            throw "Account holder name must contain only alphabets and spaces";
+        }
+        accountHolderErr.innerText = '';
+    } catch (error) {
+        accountHolderErr.innerText = error;
+        isValid = false;
+    }
+
+    try {
+        if (chequeBookType === 'Select type' || chequeBookType==='') {
+            throw "Please select cheque book type";
+        }
+        chequeBookTypeErr.innerText = '';
+    } catch (error) {
+        chequeBookTypeErr.innerText = error;
+        isValid = false;
+    }
+
+    try {
+        if (!(/^\d{10}$/).test(contactNumber)) {
+            throw "Please enter contact number";
+        }
+        contactNumberErr.innerText = '';
+    } catch (error) {
+        contactNumberErr.innerText = error;
+        isValid = false;
+    }
+
+    try {
+        if (!(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).test(email)) {
+            throw "Please enter email";
+        }
+        emailErr.innerText = '';
+    } catch (error) {
+        emailErr.innerText = error;
+        isValid = false;
+    }
+
+    return isValid;
+};
